72afe5094e8c60df399e2897b6eff30456cc7b11
Austaino <c3ono32@yahoo.com>
2019-11-27 05:00:49 -0800

    chore(deploy); github pages deploy deploy to github pages [finishes]

---
+++ src/components/layout/TNavbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const TNavbar = () => {
+  return (
+    <div id="navbar">
+      <a href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+    </div>
+  );
+};
+export default TNavbar;
