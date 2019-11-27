882fb460876944d57e399406b1092eef1172b622
Austaino <c3ono32@yahoo.com>
2019-11-27 04:57:34 -0800

    Updates

---
+++ precache-manifest.7815a863bad9a139fc656770abb88a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7ceab3f2bc7b389aeea22e2e2804da",
+    "url": "/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "bbe12e8d7fa18bf06392",
+    "url": "/teamwork-frontend/static/css/main.60c0d9be.chunk.css"
+  },
+  {
+    "revision": "c285f31b34502d5532b4",
+    "url": "/teamwork-frontend/static/js/2.8001c40c.chunk.js"
+  },
+  {
+    "revision": "bbe12e8d7fa18bf06392",
+    "url": "/teamwork-frontend/static/js/main.467e5ea7.chunk.js"
+  },
+  {
+    "revision": "c72af4ddc98ec8359cab",
+    "url": "/teamwork-frontend/static/js/runtime-main.b288330e.js"
+  },
+  {
+    "revision": "fa4eb56ac4d6de08709e807b7aac605a",
+    "url": "/teamwork-frontend/static/media/showcase.fa4eb56a.jpg"
+  }
+]);
